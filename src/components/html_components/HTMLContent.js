756b19e75369bf61fff19241f9f4a0a1fb60a97b
Evan Daley <daley.evan@gmail.com>
2021-08-19 18:39:21 -0700

    Initial commit

---
+++ src/components/html_components/HTMLContent.js
@@ -0,0 +1,21 @@
+// HTMLContent.js
+// Throw all the raw HTML content here. This is all rendered outside the context of the canvas,
+// so don't try to do anything fancy with Three.js.
+
+function Layout() {
+  return (
+    <>
+      <header className="main-header">
+
+      </header>
+    </>
+  )
+}
+
+export default function HTMLContent() {
+  return (
+    <>
+      <Layout/>
+    </>
+  )
+}
